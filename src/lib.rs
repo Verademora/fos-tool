27a05bcb192f1f278fa65f4e75c47abd0430a214
Verademora <drhax123@gmail.com>
2023-09-07 01:05:06 -0500

    large refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,106 @@
+use byteorder::{LittleEndian, ReadBytesExt};
+use std::io::{self, ErrorKind, Read};
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
+pub struct FalloutNVHeader {
+    language: String,
+    save_img_width: u32,
+    save_img_height: u32,
+    save_number: u32,
+    pc_name: String,
+    pc_title: String,
+    pc_level: u32,
+    pc_location: String,
+    playtime: String,
+}
+
+pub const MAGIC_ID_SIZE: u32 = 11;
+pub const LANGUAGE_SIZE: u32 = 64;
+const HEADER_SEPERATOR_COUNT: u32 = 14;
+
+fn read_string(size: u32, reader: &mut dyn Read) -> io::Result<String> {
+    let mut s = String::new();
+    for _ in 0..size {
+        let c = reader.read_u8()?;
+        if c != 0x00 {
+            s.push(c as char);
+        }
+    }
+
+    Ok(s)
+}
+
+impl FalloutNVHeader {
+    pub fn save_img_size(&self) -> (u32, u32) {
+        (self.save_img_width, self.save_img_height)
+    }
+
+    pub fn header_size(&self) -> u32 {
+        MAGIC_ID_SIZE + LANGUAGE_SIZE
+    }
+
+    pub fn read_from(reader: &mut dyn Read) -> Result<Self, io::Error> {
+        let magic_id = read_string(MAGIC_ID_SIZE, reader)?;
+
+        if magic_id != *"FO3SAVEGAME" {
+            let e = io::Error::new(ErrorKind::InvalidInput, "File Header error");
+            return Err(e);
+        }
+
+        let _header_size = reader.read_u32::<LittleEndian>()?;
+        let _header_mystery = reader.read_u32::<LittleEndian>()?;
+        reader.read_u8()?;
+
+        let language = read_string(LANGUAGE_SIZE, reader)?;
+        reader.read_u8()?;
+
+        let save_img_width = reader.read_u32::<LittleEndian>()?;
+        reader.read_u8()?;
+        let save_img_height = reader.read_u32::<LittleEndian>()?;
+        reader.read_u8()?;
+
+        let save_number = reader.read_u32::<LittleEndian>()?;
+        reader.read_u8()?;
+
+        let pc_name_len = reader.read_u16::<LittleEndian>()?.into();
+        reader.read_u8()?;
+        let pc_name = read_string(pc_name_len, reader)?;
+        reader.read_u8()?;
+
+        let pc_title_len = reader.read_u16::<LittleEndian>()?.into();
+        reader.read_u8()?;
+        let pc_title = read_string(pc_title_len, reader)?;
+        reader.read_u8()?;
+
+        let pc_level = reader.read_u32::<LittleEndian>()?;
+        reader.read_u8()?;
+
+        let pc_location_len = reader.read_u16::<LittleEndian>()?.into();
+        reader.read_u8()?;
+        let pc_location = read_string(pc_location_len, reader)?;
+        reader.read_u8()?;
+
+        let playtime_len = reader.read_u16::<LittleEndian>()?.into();
+        reader.read_u8()?;
+        let playtime = read_string(playtime_len, reader)?;
+        reader.read_u8()?;
+
+        Ok(Self{
+                language,
+                save_img_width,
+                save_img_height,
+                save_number,
+                pc_name,
+                pc_title,
+                pc_level,
+                pc_location,
+                playtime,
+        })
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,36 @@
-use byteorder::{LittleEndian, ReadBytesExt};
-use image::ImageBuffer;
 use std::{
     fs::File,
-    io::{self, BufReader, Read},
+    io::{self, BufReader},
 };
 
-pub const MAGIC_ID_SIZE: usize = 11;
-pub const LANGUAGE_SIZE: usize = 64;
 
-fn get_file_header(reader: &mut dyn Read) -> io::Result<()> {
-    let mut magic_id = String::new();
-    for _ in 0..MAGIC_ID_SIZE {
-        let c = reader.read_u8()?.into();
-        magic_id.push(c);
-    }
-
-    if magic_id != *"FO3SAVEGAME" {
-        panic!("Invalid save file");
-    }
-
-    let _header_size = reader.read_u32::<LittleEndian>()?;
-    let _header_mystery = reader.read_u32::<LittleEndian>()?;
-    reader.read_u8()?;
-
-    let mut language = String::new();
-    for _ in 0..LANGUAGE_SIZE {
-        let c = reader.read_u8()?;
-        if c != 0x00 {
-            language.push(c as char);
-        }
-    }
-    reader.read_u8()?;
-
-    let save_img_width = reader.read_u32::<LittleEndian>()?;
-    reader.read_u8()?;
-    let save_img_height = reader.read_u32::<LittleEndian>()?;
-    reader.read_u8()?;
-
-    let save_number = reader.read_u32::<LittleEndian>()?;
-    reader.read_u8()?;
-
-    let pc_name_len = reader.read_u16::<LittleEndian>()?;
-    reader.read_u8()?;
-    let mut pc_name = String::new();
-    for _ in 0..pc_name_len {
-        let c = reader.read_u8()?.into();
-        pc_name.push(c);
-    }
-    reader.read_u8()?;
-
-    let pc_title_len = reader.read_u16::<LittleEndian>()?;
-    reader.read_u8()?;
-    let mut pc_title = String::new();
-    for _ in 0..pc_title_len {
-        let c = reader.read_u8()?.into();
-        pc_title.push(c);
-    }
-    reader.read_u8()?;
-
-    let _pc_level = reader.read_u32::<LittleEndian>()?;
-    reader.read_u8()?;
-
-    let pc_location_len = reader.read_u16::<LittleEndian>()?;
-    reader.read_u8()?;
-    let mut pc_location = String::new();
-    for _ in 0..pc_location_len {
-        let c = reader.read_u8()?.into();
-        pc_location.push(c);
-    }
-    reader.read_u8()?;
-
-    let playtime_len = reader.read_u16::<LittleEndian>()?;
-    reader.read_u8()?;
-    let mut playtime = String::new();
-    for _ in 0..playtime_len {
-        let c = reader.read_u8()?.into();
-        playtime.push(c);
-    }
-    reader.read_u8()?;
-
-    let mut imgbuf = ImageBuffer::new(save_img_width, save_img_height);
-    for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = reader.read_u8()?;
-        let g = reader.read_u8()?;
-        let b = reader.read_u8()?;
-        *pixel = image::Rgb([r, g, b]);
-    }
-    let file_name = format!(
-        "Save_{}_{}_{}_{}.png",
-        save_number,
-        pc_name.replace(' ', "_"),
-        pc_location.replace(' ', "_"),
-        playtime.replace("00", "0").replace('.', "_")
-    );
-    imgbuf.save(file_name).unwrap();
-
-    Ok(())
-}
+// fn get_file_header(reader: &mut dyn Read) -> io::Result<()> {
+//     let mut imgbuf = ImageBuffer::new(save_img_width, save_img_height);
+//     for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
+//         let r = reader.read_u8()?;
+//         let g = reader.read_u8()?;
+//         let b = reader.read_u8()?;
+//         *pixel = image::Rgb([r, g, b]);
+//     }
+//     let file_name = format!(
+//         "Save_{}_{}_{}_{}.png",
+//         save_number,
+//         pc_name.replace(' ', "_"),
+//         pc_location.replace(' ', "_"),
+//         playtime.replace("00", "0").replace('.', "_")
+//     );
+//     imgbuf.save(file_name).unwrap();
+// 
+//     Ok(())
+// }
 
 fn main() -> io::Result<()> {
-    let file = File::open("test.fos")?;
+    let file = File::open("test4.fos")?;
     let mut reader = BufReader::new(file);
 
-    get_file_header(&mut reader)?;
+    let thing = fos_tool::FalloutNVHeader::read_from(&mut reader)?;
+    dbg!(thing);
+    // get_file_header(&mut reader)?;
 
     Ok(())
 }
